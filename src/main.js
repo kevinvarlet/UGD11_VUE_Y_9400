67fe231be51b9b8b72ce8bfd08236f38707f734b
kevinvarlet <kevinavarlet@gmail.com>
2019-11-14 15:24:27 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
-
-import {rtdbPlugin} from 'vuefire'
+import App from './App'
+import Axios from 'axios'
 import router from './router'
-
-Vue.use(rtdbPlugin)
-
+import vuetify from './plugins/vuetify';
+Vue.prototype.$http = Axios;
+Vue.prototype.$apiUrl = 'http://127.0.0.1:80/modul11/index.php';
 Vue.config.productionTip = false
-
 new Vue({
-  vuetify,
-  router,
-  render: h => h(App)
+ render: h => h(App),
+ router,
+ vuetify,
+ components: { App }
 }).$mount('#app')
